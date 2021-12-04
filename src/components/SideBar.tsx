952968eacd024fd72f3fddafe593b0c1119e7c76
vavarine <evailson.m.barbosa@gmail.com>
2021-12-04 15:21:28 -0300

    finished

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
+import { Genre } from "../App";
+import { Button } from "./Button";
+
+interface SidebarProps {
+  genres: Genre[];
+  selectedGenreId: Genre["id"];
+  handleClickButton: (id: Genre["id"]) => void;
+}
+
+export function SideBar({ genres, selectedGenreId, handleClickButton }: SidebarProps) {
   // Complete aqui
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
